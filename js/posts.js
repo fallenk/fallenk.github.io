423f7f18e0301150189e663afa7315e3c3b8122b
fallenk <kingfallentime@gmail.com>
2018-04-17 11:32:47 +0800

    Site updated: 2018-04-17 11:32:47

---
+++ js/posts.js
@@ -0,0 +1,1 @@
+[{"title":"你好，Hexo","date":"2018.04.16","uri":"2018/04/16/你好，Hexo/","excerpt":""},{"title":"Hello World","date":"2018.04.16","uri":"2018/04/16/hello-world/","excerpt":""},{"title":"第二篇博客","date":"2018.04.17","uri":"2018/04/17/第二篇博客/","excerpt":""}]
